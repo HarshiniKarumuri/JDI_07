784adfdc59cdea7e0f3d9414b84eb6a98dce3fab
Mohammad Amir <39456629+chiillax@users.noreply.github.com>
2021-01-20 21:54:49 +0530

    added comments in bean

--- CRS-JEDI-FLIPKART/src/com/flipkart/bean/Admin.java
+++ CRS-JEDI-FLIPKART/src/com/flipkart/bean/Admin.java
@@ -1,25 +1,41 @@
-/**
- * 
- */
 package com.flipkart.bean;
 
 /**
- * @author mayan
- *
+ * Admin bean class
  */
-public class Admin extends User{
-	int adminId;
-	String name;
+
+public class Admin extends User {
+
+	private int adminId;
+	private String name;
+
+	/**
+	 * @return the adminId
+	 */
+
 	public int getAdminId() {
 		return adminId;
 	}
+
+	/**
+	 * @param adminId the adminId to set
+	 */
 	public void setAdminId(int adminId) {
 		this.adminId = adminId;
 	}
+
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
+
+	/**
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+
 }
