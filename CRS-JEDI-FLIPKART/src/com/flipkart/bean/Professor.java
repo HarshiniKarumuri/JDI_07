a233caa6a713510e9e07029f74ab138111c8eaaf
Mohammad Amir <39456629+chiillax@users.noreply.github.com>
2021-01-20 21:30:57 +0530

    added comments in bean package

--- CRS-JEDI-FLIPKART/src/com/flipkart/bean/Professor.java
+++ CRS-JEDI-FLIPKART/src/com/flipkart/bean/Professor.java
@@ -7,35 +7,67 @@ package com.flipkart.bean;
  * @author mayan
  *
  */
-public class Professor extends User{
+public class Professor extends User {
 
 	int professorId;
 	String email;
 	String name;
 	int courseId;
+
+	/**
+	 * @return the professorId
+	 */
 	public int getProfessorId() {
 		return professorId;
 	}
+
+	/**
+	 * @param professorId the professorId to set
+	 */
 	public void setProfessorId(int professorId) {
 		this.professorId = professorId;
 	}
+
+	/**
+	 * @return the email
+	 */
 	public String getEmail() {
 		return email;
 	}
+
+	/**
+	 * @param email the email to set
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
+
+	/**
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	/**
+	 * @return the courseId
+	 */
 	public int getCourseId() {
 		return courseId;
 	}
+
+	/**
+	 * @param courseId the courseId to set
+	 */
 	public void setCourseId(int courseId) {
 		this.courseId = courseId;
 	}
-	
+
 }
--- CRS-JEDI-FLIPKART/src/com/flipkart/bean/User.java
+++ CRS-JEDI-FLIPKART/src/com/flipkart/bean/User.java
@@ -11,17 +11,32 @@ public class User {
 
 	int userId;
 	String username;
+
+	/**
+	 * @return the userId
+	 */
 	public int getUserId() {
 		return userId;
 	}
+
+	/**
+	 * @param userId the userId to set
+	 */
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+
+	/**
+	 * @return the username
+	 */
 	public String getUsername() {
 		return username;
 	}
+
+	/**
+	 * @param username the username to set
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
 }
