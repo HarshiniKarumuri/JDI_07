fc827097030337be6f4b520a12d5713b5161d042
HarshiniKarumuri <harshini170400@gmail.com>
2021-01-20 23:35:48 +0530

    Changed names of methods

--- CRS-JEDI-FLIPKART/src/com/flipkart/business/ProfessorOperations.java
+++ CRS-JEDI-FLIPKART/src/com/flipkart/business/ProfessorOperations.java
@@ -4,11 +4,11 @@ import com.flipkart.bean.Professor;
 
 public class ProfessorOperations implements ProfessorInterface {
 
-	public void viewCatalog() {
+	public void getCoursesTaught(Professor professor) {
 		return;
 	}
 
-	public void viewStudentRegisteredInParticularCourse(int courseId) {
+	public void viewStudents(Professor professor) {
 		return;
 	}
 
