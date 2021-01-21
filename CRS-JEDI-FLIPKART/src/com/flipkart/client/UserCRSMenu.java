30c86783cd2e785701270605464392fc58351752
saspg98 <sauravgupta97@gmail.com>
2021-01-21 10:10:21 +0530

    * UserCRSMenu added * Testing done

--- CRS-JEDI-FLIPKART/src/com/flipkart/client/UserCRSMenu.java
+++ CRS-JEDI-FLIPKART/src/com/flipkart/client/UserCRSMenu.java
@@ -3,7 +3,11 @@ package com.flipkart.client;
 import com.flipkart.bean.Professor;
 import com.flipkart.business.ProfessorOperations;
 import com.flipkart.constants.UIConstants;
+import com.flipkart.dao.LoginDAOInterface;
+import com.flipkart.dao.LoginDAOOperations;
+import com.flipkart.dao.ProfessorDAOOperations;
 
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 /**
@@ -12,6 +16,7 @@ import java.util.logging.Logger;
 public class UserCRSMenu {
 
 	private static final Logger logger = Logger.getLogger(UserCRSMenu.class.getName());
+	public static Scanner scanner = new Scanner(System.in);
 
 	// To maintain logged in/out state of the user
 	static boolean loggedIn;
@@ -22,18 +27,72 @@ public class UserCRSMenu {
 	 */
 	public static void main(String[] args) {
 		logger.info(UIConstants.WELCOME_MESSAGE);
+		UserCRSMenu.showMenu();
+		logger.info(UIConstants.EXIT_MESSAGE);
+		scanner.close();
+	}
 
-		/* //Tester
+	//Show Menu to User
+	public static void showMenu() {
+		boolean showingMenu = true;
+		int choice = 0;
+		while(showingMenu) {
+
+			try {
+				logger.info("Enter 1 to login");
+				logger.info("Enter 2 to register as a student");
+				logger.info("Enter any other number to exit");
+
+				choice = Integer.parseInt(scanner.nextLine());
+
+			} catch (Exception e) {
+				logger.info(e.getMessage());
+			}
+
+			switch(choice) {
+				case 1:
+					UserCRSMenu.login();
+					break;
+				case 2:
+					break;
+				default:
+					showingMenu = false;
+			}
+		}
+	}
+
+	//To login User
+	public static void login() {
+		logger.info("Enter Username");
+		String username = scanner.nextLine();
+		logger.info("Enter Password");
+		String password = scanner.nextLine();
+
 		loggedIn = true;
-		Professor professor = new Professor();
-		professor.setProfessorName("Mr. Aman");
-		professor.setProfessorId(2223423);
-		professor.setUserId(1233334);
-		professor.setUsername("aman.rocks");
-		professor.setEmail("aman.rocks@crshome.com");
+		LoginDAOInterface loginDAOOperations = new LoginDAOOperations();
+		String role = loginDAOOperations.login(username, password);
 
-		ProfessorCRSMenu professorCRSMenu = new ProfessorCRSMenu();
-		professorCRSMenu.displayMenu(professor);*/
+		switch(role) {
+			case "Professor":
+				logger.info("User logged in as " + role);
+				ProfessorCRSMenu professorCrsMenu = new ProfessorCRSMenu();
+				ProfessorDAOOperations professorDAOOperations = new ProfessorDAOOperations();
+				Professor professor = professorDAOOperations.getProfessorDetails(username);
+				professorCrsMenu.displayMenu(professor);
+				break;
+
+			case "Student":
+				logger.info("User logged in as " + role);
+				break;
+
+			case "Admin":
+				logger.info("User logged in as " + role);
+				break;
+				
+			default:
+				logger.info("Please Enter correct login credentials");
+				UserCRSMenu.showMenu();
+		}
 	}
 
 	/**
@@ -42,5 +101,6 @@ public class UserCRSMenu {
 	 */
 	public static void logout() {
 		loggedIn = false;
+		logger.info("User logged out");
 	}
 }
