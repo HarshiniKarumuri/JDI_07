66c206151dbd75f05d87567366102e8c8af34e16
Mohammad Amir <39456629+chiillax@users.noreply.github.com>
2021-01-25 15:20:14 +0530

    Update NotificationInterface.java

--- CRS-JEDI-FLIPKART/src/com/flipkart/service/NotificationInterface.java
+++ CRS-JEDI-FLIPKART/src/com/flipkart/service/NotificationInterface.java
@@ -3,16 +3,14 @@ package com.flipkart.service;
 import com.flipkart.bean.Notification;
 
 public interface NotificationInterface {
-	
-	/**
-	 * 
-	 * @param notification notification to be sent
-	 */
-	public void sendNotification(Notification notification);
 
-	/**
-	 * 
-	 * @param userId unique identifier of user
-	 */
-	public void getNotification(int userId);
+    /**
+     * @param notification notification to be sent
+     */
+    public void sendNotification(Notification notification);
+
+    /**
+     * @param userId unique identifier of user
+     */
+    public void getNotification(int userId);
 }
