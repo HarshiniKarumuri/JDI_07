2d1abe44c9581745bcf339dcc9dea4ccb45b430a
HarshiniKarumuri <harshini170400@gmail.com>
2021-01-25 08:30:51 +0530

    Implemented catalog dao operations using sql queries

--- CRS-JEDI-FLIPKART/src/com/flipkart/dao/CatalogDAOOperations.java
+++ CRS-JEDI-FLIPKART/src/com/flipkart/dao/CatalogDAOOperations.java
@@ -1,46 +1,61 @@
 package com.flipkart.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
+
+import org.apache.log4j.Logger;
 
 import com.flipkart.bean.Course;
+import com.flipkart.constants.SQLQueriesConstants;
+import com.flipkart.utils.DBUtils;
 
 public class CatalogDAOOperations implements CatalogDAOInterface {
+	
+	public static Logger logger = Logger.getLogger(CatalogDAOOperations.class);
+	Connection connection = DBUtils.getConnection();
 
 	@Override
 	public ArrayList<Course> viewCatalog() {
 		// TODO Auto-generated method stub
 		
-		//Dummy Data
 		ArrayList<Course> courses = new ArrayList<Course>();
+		PreparedStatement statement = null;
 		
-		Course course1 = new Course();
-		course1.setCourseId(1);
-		course1.setCourseName("Java");
-		course1.setDescription("Programming Language");
-		course1.setFees(100000);
-		courses.add(course1);
-		
-		Course course2 = new Course();
-		course2.setCourseId(2);
-		course2.setCourseName("ML");
-		course2.setDescription("A good course");
-		course2.setFees(200000);
-		courses.add(course2);
-		
+		try {
+			statement = connection.prepareStatement(SQLQueriesConstants.VIEW_COURSES_QUERY);
+			ResultSet rs = statement.executeQuery();
+			while(rs.next()) {
+				Course course = new Course();
+				course.setCourseName(rs.getString("course_name"));
+			}
+		} catch(SQLException se) {
+			logger.error(se.getMessage());
+		}
 		return courses;
 	}
 
 	@Override
 	public Course viewCourse(int courseId) {
-		// TODO Auto-generated method stub
 		
-		//Dummy Data
 		Course course1 = new Course();
-		course1.setCourseId(1);
-		course1.setCourseName("Java");
-		course1.setDescription("Programming Language");
-		course1.setFees(100000);
+		PreparedStatement statement = null;
+		
+		try {
+			statement = connection.prepareStatement(SQLQueriesConstants.VIEW_CATALOG_QUERY);
+			ResultSet rs = statement.executeQuery();
+			while(rs.next()) {
+				course1.setCourseId(courseId);
+				course1.setCourseName(rs.getString("course_name"));
+				course1.setDescription(rs.getString("descrition"));
+				course1.setFees(rs.getInt("fees"));
+				course1.setCapacity(rs.getInt("capacity"));
+			}
+		} catch(SQLException se) {
+			logger.error(se.getMessage());
+		}
 		
 		return course1;
 	}
